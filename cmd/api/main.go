e867657408c772ce7a212433aff7e50654ea7005
idorocodes <idoroyen33@gmail.com>
2026-03-01 18:09:05 +0100

    added api prefixes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,13 +10,14 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /", internal.HomePath)
-	mux.HandleFunc("GET /health", internal.HealthStatus)
-	mux.HandleFunc("POST /createuser", internal.CreateAccount)
-	mux.HandleFunc("POST /loginuser", internal.LoginAccount)
-	mux.Handle("GET /dashboard", internal.AuthMiddleware("admin", "customer")(http.HandlerFunc(internal.DashboardPath)))
-	mux.Handle("POST /createproduct", internal.AuthMiddleware("admin")(http.HandlerFunc(internal.CreateProduct)))
-	mux.Handle("GET /getProduct", internal.AuthMiddleware("admin", "customer")(http.HandlerFunc(internal.GetProducts)))
+	mux.HandleFunc("GET /api/v1", internal.HomePath)
+	mux.HandleFunc("GET /api/v1/health", internal.HealthStatus)
+	mux.HandleFunc("POST /api/v1/createuser", internal.CreateAccount)
+	mux.HandleFunc("POST /api/v1/loginuser", internal.LoginAccount)
+	mux.Handle("GET /api/v1/dashboard", internal.AuthMiddleware("admin", "customer")(http.HandlerFunc(internal.DashboardPath)))
+	mux.Handle("POST /api/v1/createproduct", internal.AuthMiddleware("admin")(http.HandlerFunc(internal.CreateProduct)))
+	mux.Handle("GET /api/v1/getProduct", internal.AuthMiddleware("admin", "customer")(http.HandlerFunc(internal.GetProducts)))
+	
 	fmt.Println("Server started on http://localhost:8080!")
 
 	http.ListenAndServe(":8080", mux)
